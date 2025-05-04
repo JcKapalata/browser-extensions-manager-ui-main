a0f1b26557f4e2349ee96ea99b372f6bda4bf1cf
JcKapalata <kapalatajc@gmail.com>
2025-05-04 13:16:45 +0200

    amelioration du styles des boites

--- src/app.js
+++ src/app.js
@@ -9,14 +9,21 @@ function addFilterAll() {
         const section = document.createElement('section');
         const btn = document.createElement('button');
         section.className = `${element.name}`;
+        if (section.className.includes(' ')) {
+            let result = section.className.replaceAll(' ', '');
+            section.className = result;
+            console.log(result);
+            
+        }
         section.innerHTML = `
-            <div>
-                <h2>${element.name}</h2>
+            <div id="container">
+                <img src="${element.source}" alt="${element.name}">
+                <div>
+                    <h2>${element.name}</h2>
+                    <p>${element.contenue}</p>
+                </div>
             </div>
-            <div>
-                <p>${element.contenue}</p>
-            </div>
-            <div>
+            <div id="btn">
                 <button> ${element.bouton}</button>
             </div>`
         filterAll.append(section)
@@ -25,6 +32,6 @@ function addFilterAll() {
 addFilterAll();
 
 // selecteur du section extension
-const sectionDevLens = document.querySelector('.DevLens');
-console.log(sectionDevLens.innerText);
+const sectionDevLens = document.querySelector('.MarkupNotes');
+console.log(sectionDevLens);
 
