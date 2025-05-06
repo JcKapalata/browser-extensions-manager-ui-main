be962e5eb02a2487ef68f83cf7caac9854443cce
JcKapalata <kapalatajc@gmail.com>
2025-05-07 00:37:10 +0200

    Ajout d'input radio au boite

--- app.js
+++ app.js
@@ -3,31 +3,55 @@ import  extensionData  from './data.js';
 // Selecteur du filterAll
 const filterAll = document.querySelector('.all');
 
+// fonction pour supprimer le espace dans le nom de la class
+function deleteEspaceClassName(name) {
+    if (name.includes(' ')) {
+        let result = name.replaceAll(' ', '');
+        return result;
+    }else{
+        return name;
+    }
+}
 
 function addFilterAll() {
     extensionData.forEach( element => {
         //creation de la section
         const section = document.createElement('section');
         section.className = `${element.name}`;
-        // condition pour supprimer les espaces dans le nom de la class s' il existe
-        if (section.className.includes(' ')) {
-            let result = section.className.replaceAll(' ', '');
-            section.className = result;
-        }
+        // appel de la fonction pour supprimer les espaces dans le nom de la class
+        section.className = deleteEspaceClassName(section.className);
 
         // creation du bouton
         const btn = document.createElement('button');
         btn.className = `${element.name}`;
-        // condition pour supprimer les espaces dans le nom de la class s' il existe
-        if (btn.className.includes(' ')) {
-            let result = btn.className.replaceAll(' ', '');
-            btn.className = result;
-        }
+        // appel de la fonction pour supprimer les espaces dans le nom de la class
+        deleteEspaceClassName(btn.className);
+        btn.className = deleteEspaceClassName(btn.className);
         btn.innerHTML = `${element.bouton}`;
+
+        //creation d'un div pour input radio
+        const divInputRadio = document.createElement('div');
+        divInputRadio.className = `${element.name}`;
+        // appel de la fonction pour supprimer les espaces dans le nom de la class
+        deleteEspaceClassName(divInputRadio.className);
+        divInputRadio.className = deleteEspaceClassName(divInputRadio.className);
+        
+        for (let index = 0; index < 2; index++) {
+            divInputRadio.innerHTML += `
+                <input type="radio" name="divInputRadio" id="radio${index} value="${element.isActive[index]}" ${element.isActive[index] ? 'checked' : ''}>
+            `;
+            
+            // if (divInputRadio.value === "true") {
+            //     divInputRadio.innerHTML = `
+            //         <input type="radio" name="divInputRadio" id="radio${index} value="${element.isActive}" checked>
+            //     `;
+            // }
+        }
 
         const div = document.createElement('div');
         div.className = `divAction`;
         div.append(btn);
+        div.append(divInputRadio);
         
         section.innerHTML = `
             <div id="container">
@@ -67,8 +91,7 @@ function btnAllfilter() {
         addFilterAll();
         // Appel de la fonction pour supprimer une extension
         removeExtension();
-    });
-//     
+    }); 
 }
 
 // Appel de la fonction pour ajouter les extensions
--- data.js
+++ data.js
@@ -3,69 +3,81 @@ const extensionsData =[
         source: "./assets/images/logo-devlens.svg",
         name: "DevLens",
         contenue:"Quickly inspect page layouts and visualize element boundaries.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   source: "./assets/images/logo-style-spy.svg",
         name: "StyleSpy",
         contenue: "Instantly analyze and copy CSS from any webpage element.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   
         source: "./assets/images/logo-speed-boost.svg",
         name: "SpeedBoost",
         contenue: "Optimizes browser resource usage to accelerate page loading.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   source: "./assets/images/logo-json-wizard.svg",
         name: "JSONWizard",
         contenue: "Formats, validates, and prettifies JSON responses in-browser.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   source: "./assets/images/logo-tab-master-pro.svg",
         name: "TabMaster Pro",
         contenue: "Organizes browser tabs into groups and sessions.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   source: "./assets/images/logo-viewport-buddy.svg",
         name: "ViewportBuddy",
         contenue: "Simulates various screen resolutions directly within the browser.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   
         source: "./assets/images/logo-markup-notes.svg",
         name: "Markup Notes",
         contenue: "Enables annotation and notes directly onto webpages for collaborative debugging.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   
         source: "./assets/images/logo-grid-guides.svg",
         name: "GridGuides",
         contenue: "Overlay customizable grids and alignment guides on any webpage.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   
         source: "./assets/images/logo-palette-picker.svg",
         name: "Palette Picker",
         contenue: "Instantly extracts color palettes from any webpage.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {   
         source: "./assets/images/logo-link-checker.svg",
         name: "LinkChecker",
         contenue: "Scans and highlights broken links on any page.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {
         source: "./assets/images/logo-dom-snapshot.svg",
         name: "DOM Snapshot",
         contenue: "Capture and export DOM structures quickly.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     },
     {
         source: "./assets/images/logo-console-plus.svg",
         name: "ConsolePlus",
         contenue: "Enhanced developer console with advanced filtering and logging.",
-        bouton: "Remove"
+        bouton: "Remove",
+        isActive: [false, true]
     }
 ];
 
